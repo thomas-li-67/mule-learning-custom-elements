31a276d2156770535935df3dc0ddc9a4b51cadf9
thomas li <thomas_li@aliyun.com>
2017-10-16 14:58:06 +0800

    Test component resolve entry point

--- src/main/java/com/mulesoft/training/MuleComponent.java
+++ src/main/java/com/mulesoft/training/MuleComponent.java
@@ -1,22 +1,43 @@
 package com.mulesoft.training;
 
 import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import org.mule.api.annotations.param.*;
 
 public class MuleComponent {
+	
 
-  public Map<String,String> processMap() {
+  public Map<String,String> processMap(Map<String,String> input) {
     // processMap implementation
-    return null;
+	  input.put("processed by","processMap");
+	  return input;    
   }
 
-  public Map<String,String> processArray() {
+  public Map<String,String> processArray(List<String> input) {
     // processArray implementation
-    return null;
+	  Map<String,String> output = new HashMap<String,String>();
+	  output.put("message", input.get(0));
+	  output.put("processedBy", "processArray");
+    return output;
   }
 
-  public Map<String,String> processString() {
+  public Map<String,String> processString(String input) {
     // processString implementation
-    return null;
+	  Map<String,String> output = new HashMap<String,String>();
+	  output.put("message", input);
+	  output.put("processedBy", "processArray");
+    return output;
   }
+  
+  public Map<String,String> processAll(@Payload Object input,@InboundHeaders("http.method") String method) {
+	    // processString implementation
+		  Map<String,String> output = new HashMap<String,String>();
+		  output.put("message", input.toString());
+		  output.put("http.method", method);
+		  output.put("processedBy","processAll");
+	    return output;
+	  } 
+ 
 
 } 
